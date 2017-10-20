dcc96c22be019eb0f753918727f35dd4dae3f14c
vedantc98 <vedantc98@gmail.com>
2017-10-20 15:59:51 +0530

    Handling multi-word queries

--- app/send_req.py
+++ app/send_req.py
@@ -4,16 +4,18 @@ import requests
 import subprocess
 import search_parser
 import os
+import string
 
 #Replace with absolute ruby path
 cwd = os.getcwd()
 RUBY_SLAVE = cwd + "/app/search_request_gen.rb"
 
-def get_search_results(searchQuery, numberOfResults,searchIndex = "All"):
+def get_search_results(searchQuery, numberOfResults, searchIndex = "All"):
 
 	temp_XML_file_path = cwd + "../" + "Responses/temp_search_results_%s.xml" %("0")
-	cmd = "ruby " + RUBY_SLAVE + " " + searchQuery + " " + searchIndex
+	cmd = "ruby " + RUBY_SLAVE + " " + string.replace(searchQuery, " ", "+") + " " + searchIndex
 	inputstr = subprocess.check_output(cmd, shell=True)
+	#print inputstr
 	inputstr = inputstr[:len(inputstr) - 1]
 	
 	r = requests.get(inputstr)
@@ -22,9 +24,11 @@ def get_search_results(searchQuery, numberOfResults,searchIndex = "All"):
 	#xmlFile = open(temp_XML_file_path, "w")
 	#xmlFile.write(responseXML)
 	#xmlFile.close()
+	#print responseXML
 
 	return search_parser.searchXMLParse(responseXML, numberOfResults)
 
 #query = raw_input().strip()
 #get_request_url(query.replace(" ", "+"))
 
+#get_search_results("iphone 8", 3)
